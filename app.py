439a8eb79423c35a5983b27a0de74e502a570c50
Lairon Souza <laionconectado@gmail.com>
2025-11-23 19:27:41 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = 'Lairon'
+    age = 32
+
+    user_data = {
+        'name': name,
+        'age': age
+    }
+
+    return jsonify(user_data)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
